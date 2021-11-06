07cd2bf9b1789789ed478c057ed61794fd78c704
Olga Koldenkova <koldolga@gmail.com>
2021-11-06 23:18:13 +0300

    feat: added task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require('fs');
+const path = require('path');
 
+
+
+function copyDir (){
+
+    fs.mkdir(path.join(__dirname, 'files-copy'), err =>{
+
+    if (err){
+        fs.readdir(path.join(__dirname, 'files-copy'), (err, elements) => {
+             if (err) throw err;
+             elements.forEach(elem => {
+                 fs.unlink(path.join(path.join(__dirname, 'files-copy'), elem), err => {
+                      if (err) throw err;
+                 });
+             })
+        });
+    }
+
+
+    fs.readdir(path.join(__dirname, 'files'),{ withFileTypes: true }, (err, elements) => {
+        if (err) throw err;
+        elements.forEach(elem => {
+          if (elem.isFile()){
+            fs.createReadStream(path.join(__dirname, 'files',elem.name)).pipe(fs.createWriteStream(path.join(__dirname, 'files-copy',elem.name)));
+          }
+        })
+    }
+    )
+    })
+}
+
+
+copyDir()
